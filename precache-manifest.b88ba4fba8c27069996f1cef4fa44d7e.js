cfb7f802fb6df9985c665e573557921ed8a99abe
Jon Bee <mr.jbee@gmail.com>
2022-07-23 05:03:24 -0500

    Initial commit

---
+++ precache-manifest.b88ba4fba8c27069996f1cef4fa44d7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4c1532034243836714d",
+    "url": "/static/css/main.92123b55.chunk.css"
+  },
+  {
+    "revision": "b4c1532034243836714d",
+    "url": "/static/js/main.b4c15320.chunk.js"
+  },
+  {
+    "revision": "0d71e04f289fc819143c",
+    "url": "/static/js/1.0d71e04f.chunk.js"
+  },
+  {
+    "revision": "4ec52039ad665b085d82",
+    "url": "/static/js/2.4ec52039.chunk.js"
+  },
+  {
+    "revision": "d1ae0facffafa29cf208",
+    "url": "/static/js/runtime~main.d1ae0fac.js"
+  },
+  {
+    "revision": "efc7fc1d4153696ae2af76328b320c92",
+    "url": "/static/media/seized.efc7fc1d.jpg"
+  },
+  {
+    "revision": "bd840631f0d8f129e952f2d968a22891",
+    "url": "/index.html"
+  }
+];
